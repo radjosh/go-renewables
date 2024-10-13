ea3b244f0b5169e8b36d692cf935f24409460a93
Joshua M Smith <radjosh@gmail.com>
2024-10-13 21:24:14 +0000

    go vet provides excellent formatting

--- src/app.go
+++ src/app.go
@@ -13,67 +13,67 @@ import (
 )
 
 type Config struct {
-	User string `env:"DB_USER,required"`
+	User     string `env:"DB_USER,required"`
 	Password string `env:"DB_PASSWORD,required"`
 }
 
 type DataPoint struct {
-	Country string
-	Year int
-	EnergyType string
-	ProductionGWh float32
-	InstalledCapacityMW float32
-	InvestmentsUSD float32
-	Population int
-	GDP float32 
-	EnergyConsumption float32
-	EnergyExports float32
-	EnergyImports float32
-	CO2Emissions float32
-	RenewableEnergyJobs int
-	GovernmentPolicies int
-	RnDExpenditure float32
-	RenewableEnergyTargets int
-	AverageAnnualTemperature float32
-	AnnualRainfall float32
-	SolarIrradiance float32
-	WindSpeed float32
-	HydroPotential float32
-	GeothermalPotential float32
-	BiomassAvailability float32
-	EnergyStorageCapacity float32
-	GridIntegrationCapability float32
-	ElectricityPrices float32
-	EnergySubsidies float32
-	InternationalAidForRenewables float32
-	PublicAwareness float32
-	EnergyEfficiencyPrograms int
-	UrbanizationRate float32
-	IndustrializationRate float32
-	EnergyMarketLiberalization int
-	RenewableEnergyPatents int
-	EducationalLevel float32
-	TechnologyTransferAgreements int
-	RenewableEnergyEducationPrograms int
-	LocalManufacturingCapacity float32
-	ImportTariffsOnEnergyEquipment float32
-	ExportIncentivesForEnergyEquipment float32
-	NaturalDisasters int
-	PoliticalStability float32
-	CorruptionPerceptionIndex float32
-	RegulatoryQuality float32
-	RuleOfLaw float32
-	ControlOfCorruption float32
-	EconomicFreedomIndex float32
-	EaseOFDoingBusiness float32
-	InnovationIndex float32
-	NumberOfResearchInstitutions int
-	NumberOFRenewableEnergyConferences int
+	Country                             string
+	Year                                int
+	EnergyType                          string
+	ProductionGWh                       float32
+	InstalledCapacityMW                 float32
+	InvestmentsUSD                      float32
+	Population                          int
+	GDP                                 float32
+	EnergyConsumption                   float32
+	EnergyExports                       float32
+	EnergyImports                       float32
+	CO2Emissions                        float32
+	RenewableEnergyJobs                 int
+	GovernmentPolicies                  int
+	RnDExpenditure                      float32
+	RenewableEnergyTargets              int
+	AverageAnnualTemperature            float32
+	AnnualRainfall                      float32
+	SolarIrradiance                     float32
+	WindSpeed                           float32
+	HydroPotential                      float32
+	GeothermalPotential                 float32
+	BiomassAvailability                 float32
+	EnergyStorageCapacity               float32
+	GridIntegrationCapability           float32
+	ElectricityPrices                   float32
+	EnergySubsidies                     float32
+	InternationalAidForRenewables       float32
+	PublicAwareness                     float32
+	EnergyEfficiencyPrograms            int
+	UrbanizationRate                    float32
+	IndustrializationRate               float32
+	EnergyMarketLiberalization          int
+	RenewableEnergyPatents              int
+	EducationalLevel                    float32
+	TechnologyTransferAgreements        int
+	RenewableEnergyEducationPrograms    int
+	LocalManufacturingCapacity          float32
+	ImportTariffsOnEnergyEquipment      float32
+	ExportIncentivesForEnergyEquipment  float32
+	NaturalDisasters                    int
+	PoliticalStability                  float32
+	CorruptionPerceptionIndex           float32
+	RegulatoryQuality                   float32
+	RuleOfLaw                           float32
+	ControlOfCorruption                 float32
+	EconomicFreedomIndex                float32
+	EaseOFDoingBusiness                 float32
+	InnovationIndex                     float32
+	NumberOfResearchInstitutions        int
+	NumberOFRenewableEnergyConferences  int
 	NumberOfRenewableEnergyPublications int
-	EnergySectorWorkforce int
-	ProportionOfEnergyFromRenewables float32
-	PublicPrivatePartnershipsInEnergy int
-	RegionalRenewableEnergyCooperation int
+	EnergySectorWorkforce               int
+	ProportionOfEnergyFromRenewables    float32
+	PublicPrivatePartnershipsInEnergy   int
+	RegionalRenewableEnergyCooperation  int
 }
 
 func connect() (*pgx.Conn, error) {
@@ -82,11 +82,11 @@ func connect() (*pgx.Conn, error) {
 	if err != nil {
 		panic(err)
 	}
-	sqlURL := "postgres://" + 
-		   cfg.User + 
-		   ":" + 
-		   cfg.Password + 
-		   "@172.18.0.3:5432/go" // url provided by docker network
+	sqlURL := "postgres://" +
+		cfg.User +
+		":" +
+		cfg.Password +
+		"@172.18.0.3:5432/go" // url provided by docker network
 	conn, err := pgx.Connect(context.Background(), sqlURL)
 	if err != nil {
 		return nil, err
@@ -163,36 +163,36 @@ func queryData(conn *pgx.Conn, dataPoints *[]DataPoint) {
 		var dataPoint DataPoint
 
 		err := rows.Scan(
-			&dataPoint.Country, 
-			&dataPoint.Year, 
-			&dataPoint.EnergyType, 
-			&dataPoint.ProductionGWh, 
-			&dataPoint.InstalledCapacityMW, 
-			&dataPoint.InvestmentsUSD, 
-			&dataPoint.Population, 
-			&dataPoint.GDP, 
-			&dataPoint.EnergyConsumption, 
-			&dataPoint.EnergyExports, 
-			&dataPoint.EnergyImports, 
-			&dataPoint.CO2Emissions, 
-			&dataPoint.RenewableEnergyJobs, 
-			&dataPoint.GovernmentPolicies, 
-			&dataPoint.RnDExpenditure, 
-			&dataPoint.RenewableEnergyTargets, 
-			&dataPoint.AverageAnnualTemperature, 
-			&dataPoint.AnnualRainfall, 
-			&dataPoint.SolarIrradiance, 
-			&dataPoint.WindSpeed, 
-			&dataPoint.HydroPotential, 
-			&dataPoint.GeothermalPotential, 
-			&dataPoint.BiomassAvailability, 
-			&dataPoint.EnergyStorageCapacity, 
-			&dataPoint.GridIntegrationCapability, 
-			&dataPoint.ElectricityPrices, 
-			&dataPoint.EnergySubsidies, 
-			&dataPoint.InternationalAidForRenewables, 
-			&dataPoint.PublicAwareness, 
-			&dataPoint.EnergyEfficiencyPrograms, 
+			&dataPoint.Country,
+			&dataPoint.Year,
+			&dataPoint.EnergyType,
+			&dataPoint.ProductionGWh,
+			&dataPoint.InstalledCapacityMW,
+			&dataPoint.InvestmentsUSD,
+			&dataPoint.Population,
+			&dataPoint.GDP,
+			&dataPoint.EnergyConsumption,
+			&dataPoint.EnergyExports,
+			&dataPoint.EnergyImports,
+			&dataPoint.CO2Emissions,
+			&dataPoint.RenewableEnergyJobs,
+			&dataPoint.GovernmentPolicies,
+			&dataPoint.RnDExpenditure,
+			&dataPoint.RenewableEnergyTargets,
+			&dataPoint.AverageAnnualTemperature,
+			&dataPoint.AnnualRainfall,
+			&dataPoint.SolarIrradiance,
+			&dataPoint.WindSpeed,
+			&dataPoint.HydroPotential,
+			&dataPoint.GeothermalPotential,
+			&dataPoint.BiomassAvailability,
+			&dataPoint.EnergyStorageCapacity,
+			&dataPoint.GridIntegrationCapability,
+			&dataPoint.ElectricityPrices,
+			&dataPoint.EnergySubsidies,
+			&dataPoint.InternationalAidForRenewables,
+			&dataPoint.PublicAwareness,
+			&dataPoint.EnergyEfficiencyPrograms,
 			&dataPoint.UrbanizationRate,
 			&dataPoint.IndustrializationRate,
 			&dataPoint.EnergyMarketLiberalization,
@@ -228,7 +228,7 @@ func queryData(conn *pgx.Conn, dataPoints *[]DataPoint) {
 }
 
 func query(w http.ResponseWriter, r *http.Request) {
-	var dataPoints []DataPoint 
+	var dataPoints []DataPoint
 
 	conn, err := connect()
 	if err != nil {
