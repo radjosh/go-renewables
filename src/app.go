d0a8480e4cdc652c1c060bcae896bbdcb41d7b7a
Joshua M Smith <radjosh@gmail.com>
2024-10-13 20:58:55 +0000

    all columns set up and working

--- src/app.go
+++ src/app.go
@@ -21,6 +21,59 @@ type DataPoint struct {
 	Country string
 	Year int
 	EnergyType string
+	ProductionGWh float32
+	InstalledCapacityMW float32
+	InvestmentsUSD float32
+	Population int
+	GDP float32 
+	EnergyConsumption float32
+	EnergyExports float32
+	EnergyImports float32
+	CO2Emissions float32
+	RenewableEnergyJobs int
+	GovernmentPolicies int
+	RnDExpenditure float32
+	RenewableEnergyTargets int
+	AverageAnnualTemperature float32
+	AnnualRainfall float32
+	SolarIrradiance float32
+	WindSpeed float32
+	HydroPotential float32
+	GeothermalPotential float32
+	BiomassAvailability float32
+	EnergyStorageCapacity float32
+	GridIntegrationCapability float32
+	ElectricityPrices float32
+	EnergySubsidies float32
+	InternationalAidForRenewables float32
+	PublicAwareness float32
+	EnergyEfficiencyPrograms int
+	UrbanizationRate float32
+	IndustrializationRate float32
+	EnergyMarketLiberalization int
+	RenewableEnergyPatents int
+	EducationalLevel float32
+	TechnologyTransferAgreements int
+	RenewableEnergyEducationPrograms int
+	LocalManufacturingCapacity float32
+	ImportTariffsOnEnergyEquipment float32
+	ExportIncentivesForEnergyEquipment float32
+	NaturalDisasters int
+	PoliticalStability float32
+	CorruptionPerceptionIndex float32
+	RegulatoryQuality float32
+	RuleOfLaw float32
+	ControlOfCorruption float32
+	EconomicFreedomIndex float32
+	EaseOFDoingBusiness float32
+	InnovationIndex float32
+	NumberOfResearchInstitutions int
+	NumberOFRenewableEnergyConferences int
+	NumberOfRenewableEnergyPublications int
+	EnergySectorWorkforce int
+	ProportionOfEnergyFromRenewables float32
+	PublicPrivatePartnershipsInEnergy int
+	RegionalRenewableEnergyCooperation int
 }
 
 func connect() (*pgx.Conn, error) {
@@ -43,7 +96,7 @@ func connect() (*pgx.Conn, error) {
 
 func queryData(conn *pgx.Conn, dataPoints *[]DataPoint) {
 	rows, err := conn.Query(context.Background(),
-		"SELECT Country, Year, EnergyType from energy")
+		"SELECT Country, Year, EnergyType, ProductionGWh, InstalledCapacityMW, InvestmentsUSD, Population, GDP, EnergyConsumption, EnergyExports, EnergyImports, CO2Emissions, RenewableEnergyJobs, GovernmentPolicies, RnDExpenditure, RenewableEnergyTargets, AverageAnnualTemperature, AnnualRainfall, SolarIrradiance, WindSpeed, HydroPotential, GeothermalPotential, BiomassAvailability, EnergyStorageCapacity, GridIntegrationCapability, ElectricityPrices, EnergySubsidies, InternationalAidForRenewables, PublicAwareness, EnergyEfficiencyPrograms, UrbanizationRate, IndustrializationRate, EnergyMarketLiberalization, RenewableEnergyPatents, EducationalLevel, TechnologyTransferAgreements, RenewableEnergyEducationPrograms, LocalManufacturingCapacity, ImportTariffsOnEnergyEquipment, ExportIncentivesForEnergyEquipment, NaturalDisasters, PoliticalStability, CorruptionPerceptionIndex, RegulatoryQuality, RuleOfLaw, ControlOfCorruption, EconomicFreedomIndex, EaseOFDoingBusiness, InnovationIndex, NumberOfResearchInstitutions, NumberOFRenewableEnergyConferences, NumberOfRenewableEnergyPublications, EnergySectorWorkforce, ProportionOfEnergyFromRenewables, PublicPrivatePartnershipsInEnergy, RegionalRenewableEnergyCooperation from energy")
 	if err != nil {
 		panic(err)
 	}
@@ -52,14 +105,68 @@ func queryData(conn *pgx.Conn, dataPoints *[]DataPoint) {
 	for rows.Next() {
 		var dataPoint DataPoint
 
-		// err := rows.Scan(&Country, &Year, &EnergyType)
-		err := rows.Scan(&dataPoint.Country, &dataPoint.Year, &dataPoint.EnergyType)
+		err := rows.Scan(
+			&dataPoint.Country, 
+			&dataPoint.Year, 
+			&dataPoint.EnergyType, 
+			&dataPoint.ProductionGWh, 
+			&dataPoint.InstalledCapacityMW, 
+			&dataPoint.InvestmentsUSD, 
+			&dataPoint.Population, 
+			&dataPoint.GDP, 
+			&dataPoint.EnergyConsumption, 
+			&dataPoint.EnergyExports, 
+			&dataPoint.EnergyImports, 
+			&dataPoint.CO2Emissions, 
+			&dataPoint.RenewableEnergyJobs, 
+			&dataPoint.GovernmentPolicies, 
+			&dataPoint.RnDExpenditure, 
+			&dataPoint.RenewableEnergyTargets, 
+			&dataPoint.AverageAnnualTemperature, 
+			&dataPoint.AnnualRainfall, 
+			&dataPoint.SolarIrradiance, 
+			&dataPoint.WindSpeed, 
+			&dataPoint.HydroPotential, 
+			&dataPoint.GeothermalPotential, 
+			&dataPoint.BiomassAvailability, 
+			&dataPoint.EnergyStorageCapacity, 
+			&dataPoint.GridIntegrationCapability, 
+			&dataPoint.ElectricityPrices, 
+			&dataPoint.EnergySubsidies, 
+			&dataPoint.InternationalAidForRenewables, 
+			&dataPoint.PublicAwareness, 
+			&dataPoint.EnergyEfficiencyPrograms, 
+			&dataPoint.UrbanizationRate,
+			&dataPoint.IndustrializationRate,
+			&dataPoint.EnergyMarketLiberalization,
+			&dataPoint.RenewableEnergyPatents,
+			&dataPoint.EducationalLevel,
+			&dataPoint.TechnologyTransferAgreements,
+			&dataPoint.RenewableEnergyEducationPrograms,
+			&dataPoint.LocalManufacturingCapacity,
+			&dataPoint.ImportTariffsOnEnergyEquipment,
+			&dataPoint.ExportIncentivesForEnergyEquipment,
+			&dataPoint.NaturalDisasters,
+			&dataPoint.PoliticalStability,
+			&dataPoint.CorruptionPerceptionIndex,
+			&dataPoint.RegulatoryQuality,
+			&dataPoint.RuleOfLaw,
+			&dataPoint.ControlOfCorruption,
+			&dataPoint.EconomicFreedomIndex,
+			&dataPoint.EaseOFDoingBusiness,
+			&dataPoint.InnovationIndex,
+			&dataPoint.NumberOfResearchInstitutions,
+			&dataPoint.NumberOFRenewableEnergyConferences,
+			&dataPoint.NumberOfRenewableEnergyPublications,
+			&dataPoint.EnergySectorWorkforce,
+			&dataPoint.ProportionOfEnergyFromRenewables,
+			&dataPoint.PublicPrivatePartnershipsInEnergy,
+			&dataPoint.RegionalRenewableEnergyCooperation,
+		)
 		if err != nil {
 			panic(err)
 		}
 		*dataPoints = append(*dataPoints, dataPoint)
-		// fmt.Printf("Country: %s, Year: %d, Energy Type: %s\n", Country, Year, EnergyType)
-		// fmt.Printf("Country: %s, Year: %d, Energy Type: %s\n", dataPoint.Country, dataPoint.Year, dataPoint.EnergyType)
 	}
 }
 
@@ -76,9 +183,6 @@ func query(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(dataPoints)
-	// for _, dataPoint := range dataPoints {
-	// 	fmt.Printf("Country: %s, Year: %d, Energy Type: %s\n", dataPoint.Country, dataPoint.Year, dataPoint.EnergyType)
-	// }
 }
 
 func main() {
